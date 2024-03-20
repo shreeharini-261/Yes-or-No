5b62e0a92cfc5f9d7f5092de15656d742ef299a2
shreeharini-261 <130243727+shreeharini-261@users.noreply.github.com>
2024-03-20 18:10:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var noButton = document.getElementById('moveButton');
+
+    document.addEventListener('mousemove', function(event) {
+        moveButton(event, noButton);
+    });
+
+    function moveButton(event, button) {
+        var mouseX = event.clientX;
+        var mouseY = event.clientY;
+
+        var buttonRect = button.getBoundingClientRect();
+        var buttonX = buttonRect.left + buttonRect.width / 2;
+        var buttonY = buttonRect.top + buttonRect.height / 2;
+
+        var distance = Math.sqrt(Math.pow(mouseX - buttonX, 2) + Math.pow(mouseY - buttonY, 2));
+
+        if (distance < 100) {
+            moveButtonToRandomPosition(button);
+        }
+    }
+
+    function moveButtonToRandomPosition(button) {
+        var randomX = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+        var randomY = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+
+        button.style.left = randomX + 'px';
+        button.style.top = randomY + 'px';
+    }
+});
+function handleYes() {
+    alert("Then what you waiting for plan it BHAKA!!<3");
+}
